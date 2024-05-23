823a859e9986514e14c3c131d388f0d5cde36843
Pawan khairnar <128224857+PawanKhairnar497@users.noreply.github.com>
2024-05-23 20:03:25 +0530

    Update if.cpp

--- Control Statement/if.cpp
+++ Control Statement/if.cpp
@@ -1,1 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number :-  ";
+    cin>>n;
+    if(n%2==0){
+        cout<<"Number is Even";
+    }
+    if(n%2!=0)
+    {
+        cout<<"Number is Odd";
+    }
+    return 0;
+}
+
 
