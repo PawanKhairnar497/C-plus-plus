5de8b3028040d91f9a92ef11ed6383fe61238934
Pawan khairnar <128224857+PawanKhairnar497@users.noreply.github.com>
2024-05-23 19:41:49 +0530

    Update SumOfNumbers.cpp

--- Input/SumOfNumbers.cpp
+++ Input/SumOfNumbers.cpp
@@ -1,1 +1,12 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cout<<"Enter the first number:- ";
+    cin>>x;
+    cout<<"Enter the second number:- ";
+    cin>>y;
+    cout<<"Sum of x & y is:- ";
+    cout<<x+y;
+    return 0;
+}
