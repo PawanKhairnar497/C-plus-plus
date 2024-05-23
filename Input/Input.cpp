7770ac830351e9c7d7cd17fbfd1cdadcccad1ada
Pawan khairnar <128224857+PawanKhairnar497@users.noreply.github.com>
2024-05-23 19:51:07 +0530

    Add files via upload

---
+++ Input/Input.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"Enter the number ";
+    cin>>x;
+    cout<<x;
+    int y = x * x;
+    cout<<endl<<"Square of the number is "<<y;
+    return 0;
+}  
