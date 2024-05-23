f5c3bd0307d919d779d3ee9847fb45a6fa01fd32
Pawan khairnar <128224857+PawanKhairnar497@users.noreply.github.com>
2024-05-23 20:09:17 +0530

    Update SolidRectangle.cpp

--- Pattern 1/SolidRectangle.cpp
+++ Pattern 1/SolidRectangle.cpp
@@ -1,1 +1,16 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+    int r;
+    cout<<"No.of rows :- ";
+    cin>>r;
+    int c;
+    cout<<"No.of columns :- ";
+    cin>>c;
+    for(int i=1;i<=r;i++){
+       for(int j=1;j<=c;j++){
+         cout<<" * "; 
+        }
+      cout<<endl;
+    }
+}
