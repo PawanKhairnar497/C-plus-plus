baeb308112d6ce2d14f330e3e6cd0cebde536de9
Pawan khairnar <128224857+PawanKhairnar497@users.noreply.github.com>
2024-05-23 20:21:51 +0530

    Create OddStarTriangle.cpp

---
+++ Pattern 2/OddStarTriangle.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"No.of rows:- ";
+    cin>>n;
+    int i,j,k;
+    for(i=1;i<=n;i++){
+        // spaces
+        for(j=1;j<=n-i;j++){
+            cout<<" ";
+        }
+        for(k=1;k<=2*i-1;k++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+//    *
+//   *** 
+//  *****
+// *******
