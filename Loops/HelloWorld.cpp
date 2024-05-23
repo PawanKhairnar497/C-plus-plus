b39f57677766bfe8d91f921f799672e508a52ccc
Pawan khairnar <128224857+PawanKhairnar497@users.noreply.github.com>
2024-05-23 20:38:25 +0530

    Create HelloWorld.cpp

---
+++ Loops/HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // n times "hello world" Take input from user
+    // Print hello word 'n' times. Take 'n' as input from user in seperate lines
+    int n ,i;
+    cout<<"Enter a number:-  ";
+    cin>>n;
+    for(i=1;i<=n;i++){
+        cout<<"Hello World"<<endl;
+    }
+}
