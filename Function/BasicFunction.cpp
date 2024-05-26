3e928595b1a15359baefb6b7c4e265f69aaac6cc
Pawan khairnar <128224857+PawanKhairnar497@users.noreply.github.com>
2024-05-26 10:58:49 +0530

    Create BasicFunction.cpp

---
+++ Function/BasicFunction.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+int main(){
+//Method 1
+    // int a,b,c;
+    // cout<<"Enter the value of a,b,c:- ";
+    // cin>>a>>b>>c;
+    // for(int i=1;i<=a;i++){
+    //     for(int j=1;j<=i;j++){
+    //         cout<<" * ";
+    //     }
+    //     cout<<endl;
+    // } 
+
+    // for(int i=1;i<=b;i++){
+    //     for(int j=1;j<=i;j++){
+    //         cout<<" * ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // for(int i=1;i<=c;i++){
+    //     for(int j=1;j<=i;j++){
+    //         cout<<" * ";
+    //     }
+    //     cout<<endl;
+    // }
+
+//Method 2
+ int a;
+ for(int k=1;k<=3;k++){
+    cin>>a;
+    for(int i=1;i<=a;i++){
+        for(int j=1;j<=i;j++){
+            cout<<" * ";
+        }
+        cout<<endl;
+    }
+ }
+return 0;
+}
