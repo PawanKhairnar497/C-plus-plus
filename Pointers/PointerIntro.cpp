7d9dc5df0f12090641f28218c36463070a2a12b9
Pawan khairnar <128224857+PawanKhairnar497@users.noreply.github.com>
2024-05-29 21:05:27 +0530

    Add files via upload

---
+++ Pointers/PointerIntro.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){ 
+
+    int x = 4;
+    int *p = &x;
+    
+    int y = 9;
+    int *q = &y;
+
+    cout<<p<<endl;
+    cout<<q;
+    cout<<endl;
+
+    float a = 9.8;
+    float *b = &a;
+    cout<<&a<<endl;
+    cout<<b;
+    cout<<endl;
+
+    char c = 'c';
+    char *d = &c;
+    cout<<&c<<endl;
+    cout<<d;
+    cout<<endl; 
+
+    bool e = true;
+    bool *f = &e;
+    cout<<&e<<endl;
+    cout<<f;
+
+}
