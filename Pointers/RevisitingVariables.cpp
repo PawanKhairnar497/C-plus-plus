22fecf5ce33c5cc577d9707c6b90413b54afa847
Pawan khairnar <128224857+PawanKhairnar497@users.noreply.github.com>
2024-05-29 21:06:02 +0530

    Add files via upload

---
+++ Pointers/RevisitingVariables.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){ 
+
+    // & operator prints address of variable
+
+    // int x ;
+    // cout<<&x<<endl;
+
+    char a = 's';
+    cout<<&a<<endl;
+
+    int x = 5;
+    int y = 5;
+    cout<<&x<<endl;
+    cout<<&y;
+
+}
