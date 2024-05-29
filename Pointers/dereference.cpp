407d478697e989c56aa4134b9f60918049900b5f
Pawan khairnar <128224857+PawanKhairnar497@users.noreply.github.com>
2024-05-29 21:06:22 +0530

    Add files via upload

---
+++ Pointers/dereference.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 12;
+    int *p = &x;
+
+    cout<<p; // address of a variable --> 0x61fe14
+    cout<<endl;
+
+    // dereference operator
+    cout<<*p; // prints the address's variable value
+   
+   // p ke andar jo address rakha hai,us address pe jao & vahan ki variable ki value print karo
+
+}    
