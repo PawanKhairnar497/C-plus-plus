2cb74ac619e2dbfb7a996d485eecd09014d33720
Pawan khairnar <128224857+PawanKhairnar497@users.noreply.github.com>
2024-05-30 20:45:58 +0530

    Add files via upload

---
+++ Pointers/Alias.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+void swap(int &a,int &b){  // Pass by reference
+    int temp = a;
+    a = b;
+    b = temp;
+    return;
+}
+
+int main(){ 
+
+    int a,b;
+    cout<<"Enter the two numbers:- ";
+    cin>>a>>b;
+
+    swap(a,b);
+    cout<<a<<" "<<b;
+
+}    
---
+++ Pointers/Arithmatic.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int x = 7;
+    // int *ptr = &x;
+    // cout<<ptr<<endl; //0x61fe14
+    //                      // cout<<*ptr<<endl;  // 7
+    // ptr = ptr + 2;
+    // cout<<ptr;  // 0x61fe1c
+
+    // Hexadecimal (16) --> 0-1-2-3-4-5-6-7-8-9  a-b-c-d-e-f  
+
+    int x = 4;
+    int *ptr = &x;
+    cout<<*ptr<<endl;  // 4
+    // (ptr)++;  --> wrong
+    *ptr = *ptr + 1;
+    // (*ptr)++;
+    cout<<*ptr<<endl;  // 6422040
+}
